8395f74cb75d51bc5583dd786616d1a494035b57
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2022-07-05 23:31:57 +0300

    Add options for sorting the news

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
-from collections import OrderedDict
 from news.models import Entity, News, NewsImage
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 # Create your views here.
 
@@ -25,14 +25,39 @@ class NewsListView(ListView):
     template_name = "news/news_list.html"
     context_object_name = "news_list"
     paginate_by = 10
-    queryset = News.objects.order_by("-pub_date")
+    # queryset = News.objects.order_by("-pub_date")
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get("q")
+        sort = self.request.GET.get("sortby")
+        order = self.request.GET.get("order")
+        object_list = News.objects.order_by("-pub_date")
+
+        if query:
+            object_list = News.objects.filter(
+                Q(title__icontains=query) | Q(body__icontains=query)
+            )
+
+        if sort and order:
+            if order == "asc":
+                object_list = object_list.order_by(sort)
+            else:
+                object_list = object_list.order_by("-" + sort)
+
+        return object_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        entities = Entity.objects.all()
+        # entities = self.object_list
+        entities = Entity.objects.filter(news__in=self.object_list)
+
+        if not entities:
+            return context
+
         for entity in entities:
             entity.count = entities.filter(name=entity.name).count()
+
         context["entity_list"] = sorted(
             remove_dupes(entities), key=lambda x: x.count, reverse=True
         )
